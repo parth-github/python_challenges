678bd5c974d418b88806a41628b42a5e39c66177
parth-github <linuxriti@gmail.com>
2022-02-02 20:28:06 +0530

    Update CompareTriplets.py

--- problem_solving/CompareTriplets.py
+++ problem_solving/CompareTriplets.py
@@ -1,11 +1,5 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
 #
 # Complete the 'compareTriplets' function below.
 #
@@ -29,17 +23,11 @@ def compareTriplets(a, b):
     return score
 
 if __name__ == '__main__':
-    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     a = list(map(int, input().rstrip().split()))
 
     b = list(map(int, input().rstrip().split()))
 
     result = compareTriplets(a, b)
-
-    #fptr.write(' '.join(map(str, result)))
-    #fptr.write('\n')
-
-    #fptr.close()
     
     print(result)
